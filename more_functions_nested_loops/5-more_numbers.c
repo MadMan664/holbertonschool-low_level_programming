4c8a52553162e8da0fa5e6411156de14d916fad8
MadMan664 <madhi704@gmail.com>
2026-07-08 17:08:25 +0300

    Fix 5-more_numbers: reduce to exactly 3 _putchar call sites

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 		j = 0;
 		while (j <= 14)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		_putchar('\n');
